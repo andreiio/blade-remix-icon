d3c1c3706c3b71cb85b66e017143d1ae166f5600
Andrei Ioniță <hi@andrei.io>
2023-04-21 12:15:11 +0100

    Revert "update service provider"
    This reverts commit e1a76747cbbf9afb14bde184e8c3ede946556e69.

--- src/BladeRemixIconServiceProvider.php
+++ src/BladeRemixIconServiceProvider.php
@@ -13,27 +13,8 @@ class BladeRemixIconServiceProvider extends ServiceProvider
     {
         $this->callAfterResolving(Factory::class, function (Factory $factory) {
             $factory->add('remix', [
+                'path' => __DIR__.'/../resources/svg',
                 'prefix' => 'ri',
-                'paths' => [
-                    __DIR__.'/../resources/svg/Arrows',
-                    __DIR__.'/../resources/svg/Buildings',
-                    __DIR__.'/../resources/svg/Business',
-                    __DIR__.'/../resources/svg/Communication',
-                    __DIR__.'/../resources/svg/Design',
-                    __DIR__.'/../resources/svg/Development',
-                    __DIR__.'/../resources/svg/Device',
-                    __DIR__.'/../resources/svg/Document',
-                    __DIR__.'/../resources/svg/Editor',
-                    __DIR__.'/../resources/svg/Finance',
-                    __DIR__.'/../resources/svg/Health & Medical',
-                    __DIR__.'/../resources/svg/Logos',
-                    __DIR__.'/../resources/svg/Map',
-                    __DIR__.'/../resources/svg/Media',
-                    __DIR__.'/../resources/svg/Others',
-                    __DIR__.'/../resources/svg/System',
-                    __DIR__.'/../resources/svg/User & Faces',
-                    __DIR__.'/../resources/svg/Weather',
-                ],
             ]);
         });
     }
